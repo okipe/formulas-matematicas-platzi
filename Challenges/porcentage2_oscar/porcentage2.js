665e2e56d2fcde3a92e282eedc45410755b68434
Oscar Roman <oscarroman.pe@gmail.com>
2023-03-13 13:27:35 -0500

    new discount

---
+++ Challenges/porcentage2_oscar/porcentage2.js
@@ -0,0 +1,6 @@
+const inputPrice = document.querySelector('#price');
+const inputDiscount = document.querySelector('#discount');
+const btn = document.querySelector('#calculate');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener("click", calcularPrecioConDescuento);
