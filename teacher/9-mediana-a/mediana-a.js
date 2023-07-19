d23b0b310b5bdc6101ee10f502dd8986b4f763bb
Oscar Roman <oscarroman.pe@gmail.com>
2023-07-19 08:30:55 -0500

    mediana

---
+++ teacher/9-mediana-a/mediana-a.js
@@ -0,0 +1,18 @@
+function esPar(lista) {
+    if (lista.length % 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+    
+    
+    // Usa la función reduce de los arrays en JavaScript para sumar todos los elementos de la lista
+    // reduce toma una función de flecha que suma los elementos actuales (a y b) de la lista
+    // Esto se hace en una sola línea, lo que hace este código más conciso que el anterior const sumaLista = lista.reduce((a, b) => a + b);
+    // const sumaLista = lista.reduce((a, b) => a + b);
+    
+    // const promedio = sumaLista / lista.length;
+    // console.log(promedio);
+    // return promedio;
+ 
