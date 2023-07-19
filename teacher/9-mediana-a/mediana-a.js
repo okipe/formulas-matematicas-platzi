eda3c374765f4bcdaeedf6c2ec4763aa88ed1cba
Oscar Roman <oscarroman.pe@gmail.com>
2023-07-19 14:13:21 -0500

    Mediana JS

--- teacher/9-mediana-a/mediana-a.js
+++ teacher/9-mediana-a/mediana-a.js
@@ -1,23 +1,40 @@
-function esPar(lista) {
-    /* // método básico
-    if (lista.length % 2) {
-        return false;
-    } else {
-        return true;
-    } */
+/* // método básico
+if (lista.length % 2) {
+    return false;
+} else {
+    return true;
+} */
 
+// Función para verificar si la longitud de la lista es par
+function esPar(lista) {
+    // Devuelve true si es par (sin residuo) y false si es impar, 
     return !(lista.length % 2);
 }
 
+// Función para verificar si la longitud de la lista es impar
 function esImpar(lista) {
+    // Devuelve 1 si es impar, 0 si es par.
     return lista.length % 2;
 }
 
 function calcularMediana(lista) {
+
+    // Verificamos si la longitud de la lista es par
     const listaEsPar = esPar(lista);
 
     if (listaEsPar) {
-        // Probar cómo calcular lista par
+        const indexMitad1ListaPar = (lista.length / 2) - 1;
+        const indexMitad2ListaPar = lista.length / 2;
+
+        // calcularPromedio( [ lista[indexMitad1ListaPar], lista[indexMitad2ListaPar] ] );
+        const listaMitades = [];
+        listaMitades.push(lista[indexMitad1ListaPar]); // Así es más facil agregar elementos.
+        listaMitades.push(lista[indexMitad2ListaPar]);
+        
+        // calcularPromedio(listaMitades);
+        const medianaListaPar = calcularPromedio(listaMitades);
+        return medianaListaPar;
+    
     } else {
         const indexMitadListaImpar = Math.floor(lista.length / 2);
         const medianaListaImpar = lista[indexMitadListaImpar]; 
@@ -25,9 +42,19 @@ function calcularMediana(lista) {
         console.log(medianaListaImpar);
     }
 }
+ 
+function calcularPromedio(lista) {
+    function sumarTodosElementos(valorAcumulado, nuevoValor) {
+        return valorAcumulado + nuevoValor;
+    }
+
+    const sumaLista = lista.reduce(sumarTodosElementos);
+    const promedio = sumaLista / lista.length;
+    // console.log(promedio);
+    return promedio;
+}
     
-    
-    // Usa la función reduce de los arrays en JavaScript para sumar todos los elementos de la lista
+    // Usa la función 'reduce' de los arrays en JavaScript para sumar todos los elementos de la lista
     // reduce toma una función de flecha que suma los elementos actuales (a y b) de la lista
     // Esto se hace en una sola línea, lo que hace este código más conciso que el anterior const sumaLista = lista.reduce((a, b) => a + b);
     // const sumaLista = lista.reduce((a, b) => a + b);
